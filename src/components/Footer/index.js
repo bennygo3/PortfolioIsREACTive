c1e98856bdd921c7d618974a2b22541f7bd0654d
Benjamin Gomez <3gobenjamin@gmail.com>
2022-04-04 13:23:31 -0500

    changes to Footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,28 +4,28 @@ import './Footer.css';
 class Footer extends React.Component {
     render() {
         return (
-            <div>
-                <div className="buttons">
-                    <div className="button">
-                        <a href="https://github.com/bennygo3" rel="noreferrer" target="_blank">
-                            {/* <i className="github"></i> */}
-                            <p>Github</p>
-                        </a>
-                    </div>
-                    <div className="button">
-                        <a href="https://www.linkedin.com/in/benjamin-a-gomez" rel="noreferrer" target="_blank">
-                            {/* <i className="linkedin"></i> */}
-                            <p>LinkedIn</p>
-                        </a>
-                    </div>
-                    <div className="button">
-                        <a href="mailto:gobecodeme@gmail.com" rel="noreferrer" target="_blank">
-                            {/* <i className="email"></i> */}
-                            <p>Email</p>
-                        </a>
-                    </div>
+
+            <div className="buttons">
+                <div className="button">
+                    <a href="https://github.com/bennygo3" rel="noreferrer" target="_blank">
+                        <i className='fa fa-github red-color'></i>
+                        {/* <p>Github</p> */}
+                    </a>
+                </div>
+                <div className="button">
+                    <a href="https://www.linkedin.com/in/benjamin-a-gomez" rel="noreferrer" target="_blank">
+                        <i className="fab fa-linkedin"></i>
+                        {/* <p>LinkedIn</p> */}
+                    </a>
+                </div>
+                <div className="button">
+                    <a href="mailto:gobecodeme@gmail.com" rel="noreferrer" target="_blank">
+                    <i className="fa-regular fa-envelope"></i>
+                        {/* <p>Email</p> */}
+                    </a>
                 </div>
             </div>
+
         );
     }
 }
